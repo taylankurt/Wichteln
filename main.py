b788eaa911b61aa4b96c1f300c419a04cb531018
Taylan Kurt <kurt.taylan@icloud.com>
2022-11-11 17:21:17 +0100

    the wichteln_auto on test.py is translated to Turkish for convenience

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def wichteln_auto():
     names = ["Taylan", "Tacim", "Ezgi", "Serhat", "Gülistan", "Muhi"]
     to_be_drawn = ["Taylan", "Tacim", "Ezgi", "Serhat", "Gülistan", "Muhi"]
     while draw_count < 6:
-        enter_first = input("{} please press ENTER: ".format(names[0]))
+        enter_first = input(
+            "Hi {}, press ENTER to get your match: ".format(names[0]))
         current_user = names[0]
         names.remove(current_user)
         if [current_user] == to_be_drawn:
@@ -53,14 +54,15 @@ def wichteln_auto():
             to_be_drawn.append(current_user)
         else:
             random_x = random.choice(to_be_drawn)
-        print("{} you have {}".format(current_user, random_x))
+        print("{} you have drawn: {}. Please note the name.".format(
+            current_user, random_x))
         random_selected = random_x
         to_be_drawn.remove(random_selected)
-        next_user = input("Please press ENTER: ")
+        next_user = input(
+            "When ready press ENTER to wipe the screen: ")
         os.system("cls")
         draw_count = draw_count + 1
     return "Mischief managed!"
 
 
-print("test")
 print(wichteln_auto())
--- test.py
+++ test.py
@@ -1,5 +1,36 @@
-names = "Taylan"
-to_be_drawn = ["Taylan"]
+import random
+import os
 
-if [names] == to_be_drawn:
-    print(names)
+
+def wichteln_auto():
+    os.system("cls")
+    draw_count = 0
+    names = ["Taylan", "Tacım", "Ezgi", "Serhat", "Gülistan", "Muhi"]
+    to_be_drawn = ["Taylan", "Tacım", "Ezgi", "Serhat", "Gülistan", "Muhi"]
+    while draw_count < 6:
+        enter_first = input(
+            "Merhaba {}, isim çekmek için ENTER tuşuna bas: ".format(names[0]))
+        current_user = names[0]
+        names.remove(current_user)
+        if [current_user] == to_be_drawn:
+            print("Kendini çektin, lütfen baştan başla!")
+            start_over = input("Bitirmek için ENTER tuşuna bas: ")
+            return "Muziplik tamamlanmadı :("
+        elif current_user in to_be_drawn:
+            to_be_drawn.remove(current_user)
+            random_x = random.choice(to_be_drawn)
+            to_be_drawn.append(current_user)
+        else:
+            random_x = random.choice(to_be_drawn)
+        print("{} sana {} çıktı. Lütfen ismi not et.".format(
+            current_user, random_x))
+        random_selected = random_x
+        to_be_drawn.remove(random_selected)
+        next_user = input(
+            "Ekranı silmek için hazır olduğunda ENTER tuşuna bas: ")
+        os.system("cls")
+        draw_count = draw_count + 1
+    return "Muziplik tamamlandı!"
+
+
+print(wichteln_auto())
